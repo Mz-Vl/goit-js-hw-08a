583aea1f0749698573e75f0882016f550422fb9c
Vladyslav Mazur <vladopermoz@gmail.com>
2024-05-27 21:22:23 +0300

    Add pages and fixed form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,15 +2,15 @@ import throttle from "lodash.throttle";
 
 
 const form = document.querySelector(".feedback-form");
-const email = document.querySelector("input[name='email']");
-const message = document.querySelector("textarea[name='message']");
+const emailInput = document.querySelector("input[name='email']");
+const messageInput = document.querySelector("textarea[name='message']");
 
 const STORAGE_KEY = "feedback-form-state";
 
 const saveFormState = throttle(() => {
     const formData = {
-        email: email.value,
-        message: message.value,
+        email: emailInput.value,
+        message: messageInput.value,
     };
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }, 500);
@@ -20,8 +20,8 @@ const loadFormState = () => {
     const savedState = localStorage.getItem(STORAGE_KEY);
     if (savedState) {
         const { email, message } = JSON.parse(savedState);
-        email.value = email;
-        message.value = message;
+        emailInput.value = email;
+        messageInput.value = message;
     }
 };
 
@@ -31,8 +31,8 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const formData = {
-        email: email.value,
-        message: message.value,
+        email: emailInput.value,
+        message: messageInput.value,
     };
 
     console.log(formData);
